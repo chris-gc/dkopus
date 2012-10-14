a85079b2a400ee0a36d3971c96ef80dabc2981b8
Christopher A. Taylor <chris@gameclosure.com>
2012-10-13 23:19:12 -0700

    add site

--- devjs_compile.sh
+++ devjs_compile.sh
@@ -6,3 +6,11 @@ cd ..
 cat js/require.js > js/land.min.js
 cat requirejs-jquery-build/main.js >> js/land.min.js
 
+mkdir -p site/css
+mkdir -p site/js
+
+cp land.html site
+cp css/screen.css site/css/screen.css
+cp css/ie.css site/css/ie.css
+cp js/land.min.js site/js/land.min.js
+
--- final_compile.sh
+++ final_compile.sh
@@ -7,3 +7,10 @@ cd ..
 
 java -jar /usr/local/google/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js js/require.js --js requirejs-jquery-build/main.js > js/land.min.js
 
+mkdir -p site/css
+mkdir -p site/js
+
+cp land.html site
+cp css/screen.css site/css/screen.css
+cp css/ie.css site/css/ie.css
+cp js/land.min.js site/js/land.min.js
